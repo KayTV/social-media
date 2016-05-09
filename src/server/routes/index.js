1036e5e663cd7aca8dcd61640b58899866332211
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-05-09 13:07:11 -0600

    setting up the angular frontend

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,8 +1,55 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../../../db/knex');
+var passport = require('../lib/auth');
+var helpers = require('../lib/helpers');
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user) {
+    if (err) {
+      res.json({title: 'Error', errors: ['Email and/or password incorrect']})
+    } else {
+      req.logIn(user, function(err) {
+        if (err) {
+          return next(err);
+        } else {
+          return res.json({status: 'login', user: user});
+        }
+      });
+    }
+  })(req, res, next);
 });
 
-module.exports = router;
+router.post('/register', function(req, res, next) {
+  var email = req.body.email;
+  var password = req.body.password;
+  knex('users').where('email', email)
+    .then(function(data){
+      // if email is in the database send an error
+      if(data.length) {
+        res.json({title: 'Error', errors: ['Email already exists']})
+      } else {
+        // hash and salt the password
+        var hashedPassword = helpers.hashing(password);
+        // if email is not in the database insert it
+        knex('users').insert({
+          email: email,
+          password: hashedPassword
+        })
+        .then(function(data) {
+          res.json({message: 'successfully registered, please login!', status: 200})
+        })
+        .catch(function(err) {
+          return res.json('crap');
+        });
+      }
+    })
+    .catch(function(err){
+      return next(err);
+    });
+});
+
+router.get('/logout', function(req, res, next) {
+  req.logout();
+  res.status(200).json({status: 'Bye'});
+});
